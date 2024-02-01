d763a89f6c9c1a9d5d324a5bf7e6dde54b013709
David Schoemehl <davidschoemehl@gmail.com>
2024-01-31 23:06:02 -0600

    Spatial binning and colors

--- compute_shaders/boid_simulation.glsl
+++ compute_shaders/boid_simulation.glsl
@@ -3,33 +3,12 @@
 
 layout(local_size_x = 1024, local_size_y = 1, local_size_z = 1) in;
 
-layout(set = 0, binding = 0, std430) restrict buffer Position {
-    vec2 data[];
-} boid_pos;
-
-layout(set = 0, binding = 1, std430) restrict buffer Velocity{
-    vec2 data[];
-} boid_vel;
-
-layout(set = 0, binding = 2, std430) restrict buffer Params{
-    float num_boids;
-    float image_size;
-    float friend_radius;
-    float avoid_radius;
-    float min_vel;
-    float max_vel;
-    float alignment_factor;
-    float cohesion_factor;
-    float separation_factor;
-    float viewport_x;
-    float viewport_y;
-    float delta_time;
-} params;
-
-layout(rgba16f, binding = 3) uniform image2D boid_data;
+#include "shared_data.glsl"
 
 void main() {
     int my_index = int(gl_GlobalInvocationID.x);
+    if(my_index >= params.num_boids) return;
+
     vec2 my_pos = boid_pos.data[my_index];
     vec2 my_vel = boid_vel.data[my_index];
     vec2 avg_vel = vec2(0,0);
@@ -38,6 +17,56 @@ void main() {
     int avoids = 0;
     int num_friends = 0;
 
+    bool use_bins = true;
+    int my_bin = bin.data[my_index];
+
+    int color_mode = int(params.color_mode);
+    
+if(use_bins)
+{
+    vec2 my_bin_x_y = one_to_two(my_bin, bin_params.bins_x);
+    vec2 starting_bin = my_bin_x_y - vec2(1,1);
+    vec2 current_bin = starting_bin;
+    for(int y = 0; y < 3; y++){
+        current_bin.y = starting_bin.y + y;
+        if(current_bin.y < 0 || current_bin.y > bin_params.bins_y) continue;
+
+        for(int x = 0; x < 3; x++){
+            current_bin.x = starting_bin.x + x;
+            if(current_bin.x < 0 || current_bin.x > bin_params.bins_x) continue;
+
+            int bin_index = two_to_one(current_bin, bin_params.bins_x);
+            for(int i = bin_prefix_sum.data[bin_index - 1]; i < bin_prefix_sum.data[bin_index]; i++){
+
+                int detection_type = 1;
+
+                int other_index = bin_reindex.data[i];
+                if(other_index != my_index){
+                    vec2 other_pos = boid_pos.data[other_index];
+                    vec2 other_vel = boid_vel.data[other_index];
+                    float dist = distance(my_pos, other_pos);
+                    if(dist < params.friend_radius){
+                        detection_type = 2;
+                        num_friends += 1;
+                        avg_vel += other_vel;
+                        midpoint += other_pos;
+                        if(dist < params.avoid_radius) {
+                            detection_type = 3;
+                            avoids += 1;
+                            separation_vec += my_pos - other_pos;
+                        }
+                    }
+                    if(my_index == 0 && color_mode == 4){
+                        ivec2 pixel_pos = ivec2(int(mod(other_index, params.image_size)), int(other_index / params.image_size));
+                        vec4 pos_rot = imageLoad(boid_data, pixel_pos);
+                        imageStore(boid_data, pixel_pos, vec4(pos_rot.x, pos_rot.y, pos_rot.z, detection_type));
+                    }
+                } 
+            }
+        }
+    }
+}
+else{
     for(int i = 0; i < params.num_boids; i++)
     {
         if(i != my_index){
@@ -55,6 +84,7 @@ void main() {
             }
         }
     }
+}
     if(num_friends > 0)
     {
         avg_vel /= num_friends;
@@ -83,10 +113,37 @@ void main() {
     my_pos += my_vel * params.delta_time;
     my_pos = vec2(mod(my_pos.x, params.viewport_x), mod(my_pos.y, params.viewport_y));
 
-    boid_vel.data[my_index] = my_vel;
-    boid_pos.data[my_index] = my_pos;
-
+    if(!bool(params.pause))
+    {
+        boid_vel.data[my_index] = my_vel;
+        boid_pos.data[my_index] = my_pos;
+    }
+    bin.data[my_index] = int(my_pos.x / bin_params.bin_size) + int(my_pos.y / bin_params.bin_size) * bin_params.bins_x;
+    
     ivec2 pixel_pos = ivec2(int(mod(my_index, params.image_size)), int(my_index / params.image_size));
 
-    imageStore(boid_data, pixel_pos,vec4(my_pos.x, my_pos.y, my_rot, 0));
+    switch(color_mode){
+        case 0:
+        case 1:
+        case 2:
+            imageStore(boid_data, pixel_pos,vec4(my_pos.x, my_pos.y, my_rot, num_friends));
+            break;
+        case 3:
+            int bin_even_odd_row_col = (bin.data[my_index] % 2 + int(bin.data[my_index] / float(bin_params.bins_x))) % 2;
+            if(bin_params.bins_x % 2 == 1)
+            {
+                bin_even_odd_row_col = bin.data[my_index] % 2;
+            }
+
+            imageStore(boid_data, pixel_pos,vec4(my_pos.x, my_pos.y, my_rot, bin_even_odd_row_col));
+            break;
+        case 4:
+            vec4 pos_rot = imageLoad(boid_data, pixel_pos);
+            int detection_type = int(pos_rot.a);
+            if(my_index == 0){
+                detection_type = 4;
+            }
+            imageStore(boid_data, pixel_pos, vec4(my_pos.x, my_pos.y, my_rot, detection_type));
+            break;
+    }
 }
--- main/main.gd
+++ main/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var DEBUG_LOG = false
 
-var NUM_BOIDS = 1000
+var NUM_BOIDS = 50000
 var boid_pos = []
 var boid_vel = []
 
@@ -9,19 +9,57 @@ var IMAGE_SIZE = int(ceil(sqrt(NUM_BOIDS)))
 var boid_data : Image
 var boid_data_texture : ImageTexture
 
-@export_range(0, 50) var friend_radius = 30.0
-@export_range(0, 50) var avoid_radius = 15.0
-@export_range(0,100) var min_vel = 25.0
-@export_range(0,100) var max_vel = 50.0
+@export_category("Boid Settings")
+@export_range(0, 50) var friend_radius = 10.0
+@export_range(0, 50) var avoid_radius = 5.0
+@export_range(0,100) var min_vel = 50.0
+@export_range(0,100) var max_vel = 75.0
 @export_range(0,100) var alignment_factor = 10.0
 @export_range(0,100) var cohesion_factor = 1.0
-@export_range(0,100) var separation_factor = 2.0
+@export_range(0,100) var separation_factor = 20.0
+
+@export_category("Rendering")
+@export var boid_color = Color(Color.WHITE) :
+	set(new_color):
+		boid_color = new_color
+		if is_inside_tree():
+			$BoidParticles.process_material.set_shader_parameter("color", boid_color)
+
+enum BoidColorMode {SOLID, HEADING, FRIENDS, BIN, DETECTION}
+@export var boid_color_mode : BoidColorMode :
+	set(new_color_mode):
+		boid_color_mode = new_color_mode
+		if is_inside_tree():
+			$BoidParticles.process_material.set_shader_parameter("color_mode", boid_color_mode)
+			
+@export var boid_max_friends = 10 :
+	set(new_max_friends):
+		boid_max_friends = new_max_friends
+		if is_inside_tree():
+			$BoidParticles.process_material.set_shader_parameter("max_friends", boid_max_friends)
+
+@export_range(.1, 1.0) var boid_scale = .5:
+	set(new_scale):
+		boid_scale = new_scale
+		if is_inside_tree():
+			$BoidParticles.process_material.set_shader_parameter("scale", boid_scale)
+
+@export var bin_grid = false:
+	set(new_grid):
+		bin_grid = new_grid
+		if is_inside_tree():
+			$Grid.visible = bin_grid
+
+@export_category("Other")
+@export var pause = false :
+	set(new_value):
+		pause = new_value
 
 # GPU Variables
 var SIMULATE_GPU = true
 var rd : RenderingDevice
 var boid_compute_shader : RID
-var pipeline : RID
+var boid_pipeline : RID
 var bindings : Array
 var uniform_set : RID
 
@@ -31,9 +69,44 @@ var params_buffer: RID
 var params_uniform : RDUniform
 var boid_data_buffer : RID
 
+# BIN Variable
+var BIN_SIZE = 128
+var BINS = Vector2i.ZERO
+var NUM_BINS = 0
+
+var bin_sum_shader : RID
+var bin_sum_pipeline : RID
+var bin_prefix_sum_shader : RID
+var bin_prefix_sum_pipeline : RID
+var bin_reindex_shader : RID
+var bin_reindex_pipeline : RID
+
+var bin_buffer : RID
+var bin_sum_buffer : RID
+var bin_prefix_sum_buffer : RID
+var bin_index_tracker_buffer : RID
+var bin_reindex_buffer : RID
+var bin_params_buffer : RID
+
 func _ready():
 	boid_data = Image.create(IMAGE_SIZE, IMAGE_SIZE, false, Image.FORMAT_RGBAH)								
 	boid_data_texture = ImageTexture.create_from_image(boid_data)
+	
+	boid_color = boid_color
+	boid_color_mode = boid_color_mode
+	boid_max_friends = boid_max_friends
+	boid_scale = boid_scale
+	bin_grid = bin_grid
+	
+	BINS = Vector2i(snapped(get_viewport_rect().size.x / BIN_SIZE + .4,1),
+					snapped(get_viewport_rect().size.y / BIN_SIZE + .4,1))
+	NUM_BINS = BINS.x * BINS.y
+	
+	$Grid.bin_size = BIN_SIZE
+	$Grid.bins_x = BINS.x
+	$Grid.bins_y = BINS.y
+	
+	print(NUM_BINS)
 	
 	_generate_boids()
 	
@@ -56,18 +129,19 @@ func _generate_boids():
 func _process(delta):	
 	get_window().title = "GPU: " + str(SIMULATE_GPU) + " / Boids: " + str(NUM_BOIDS) + " / FPS: " + str(Engine.get_frames_per_second())
 	
-	get_window().title = "Boids: " + str(NUM_BOIDS) + " / FPS: " + str(Engine.get_frames_per_second())
-
 	if SIMULATE_GPU:
 		_sync_boids_gpu()
 	else:
 		_update_boids_cpu(delta)
 		
+	#var test_output = rd.buffer_get_data(bin_sum_buffer).to_int32_array()
+	#print("bin_sum",test_output)
+		
 	_update_data_texture()
 
 	if SIMULATE_GPU:
 		_update_boids_gpu(delta)
-		
+				
 func _update_boids_cpu(delta):
 	for i in NUM_BOIDS:
 		var my_pos = boid_pos[i]
@@ -117,10 +191,19 @@ func _update_boids_gpu(delta):
 	params_uniform.add_id(params_buffer)
 	uniform_set = rd.uniform_set_create(bindings, boid_compute_shader, 0)
 	
+	_run_compute_shader(bin_sum_pipeline)
+	rd.sync()
+	_run_compute_shader(bin_prefix_sum_pipeline)
+	rd.sync()
+	_run_compute_shader(bin_reindex_pipeline)
+	rd.sync()
+	
+	_run_compute_shader(boid_pipeline)
+
+func _run_compute_shader(pipeline):
 	var compute_list := rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-	
 	rd.compute_list_dispatch(compute_list, ceil(NUM_BOIDS/1024.), 1, 1)
 	rd.compute_list_end()
 	rd.submit()
@@ -146,7 +229,7 @@ func _setup_compute_shader():
 	var shader_file := load("res://compute_shaders/boid_simulation.glsl")
 	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
 	boid_compute_shader = rd.shader_create_from_spirv(shader_spirv)
-	pipeline = rd.compute_pipeline_create(boid_compute_shader)
+	boid_pipeline = rd.compute_pipeline_create(boid_compute_shader)
 	
 	boid_pos_buffer = _generate_vec2_buffer(boid_pos)
 	var boid_pos_uniform = _generate_uniform(boid_pos_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 0)
@@ -167,13 +250,63 @@ func _setup_compute_shader():
 	boid_data_buffer = rd.texture_create(fmt, view, [boid_data.get_data()])
 	var boid_data_buffer_uniform = _generate_uniform(boid_data_buffer, RenderingDevice.UNIFORM_TYPE_IMAGE, 3)
 	
-	bindings = [boid_pos_uniform, boid_vel_uniform, params_uniform,boid_data_buffer_uniform]
+	shader_file = load("res://compute_shaders/bin_sum.glsl")
+	shader_spirv = shader_file.get_spirv()
+	bin_sum_shader = rd.shader_create_from_spirv(shader_spirv)
+	bin_sum_pipeline = rd.compute_pipeline_create(bin_sum_shader)
+	
+	shader_file = load("res://compute_shaders/bin_prefix_sum.glsl")
+	shader_spirv = shader_file.get_spirv()
+	bin_prefix_sum_shader = rd.shader_create_from_spirv(shader_spirv)
+	bin_prefix_sum_pipeline = rd.compute_pipeline_create(bin_prefix_sum_shader)
+	
+	shader_file = load("res://compute_shaders/bin_reindex.glsl")
+	shader_spirv = shader_file.get_spirv()
+	bin_reindex_shader = rd.shader_create_from_spirv(shader_spirv)
+	bin_reindex_pipeline = rd.compute_pipeline_create(bin_reindex_shader)
+	
+	var bin_params_buffer_bytes = PackedInt32Array([BIN_SIZE, BINS.x, BINS.y, NUM_BINS]).to_byte_array()
+	bin_params_buffer = rd.storage_buffer_create(bin_params_buffer_bytes.size(), bin_params_buffer_bytes)
+	var bin_params_uniform = _generate_uniform(bin_params_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 4)
+		
+	bin_buffer = _generate_int_buffer(NUM_BOIDS)
+	var bin_buffer_uniform = _generate_uniform(bin_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 5)
+		
+	bin_sum_buffer = _generate_int_buffer(NUM_BINS)
+	var bin_sum_uniform = _generate_uniform(bin_sum_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 6)
+	
+	bin_prefix_sum_buffer = _generate_int_buffer(NUM_BINS)
+	var bin_prefix_sum_uniform = _generate_uniform(bin_prefix_sum_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 7)
+	
+	bin_index_tracker_buffer = _generate_int_buffer(NUM_BINS)
+	var bin_index_tracker_uniform = _generate_uniform(bin_index_tracker_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 8)
+	
+	bin_reindex_buffer = _generate_int_buffer(NUM_BOIDS)
+	var  bin_reindex_uniform = _generate_uniform(bin_reindex_buffer, RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER, 9)
+	
+	bindings = [boid_pos_uniform, 
+				boid_vel_uniform, 
+				params_uniform,
+				boid_data_buffer_uniform,
+				bin_params_uniform,
+				bin_buffer_uniform,
+				bin_sum_uniform,
+				bin_prefix_sum_uniform,
+				bin_index_tracker_uniform,
+				bin_reindex_uniform]
 	
 func _generate_vec2_buffer(data):
 	var data_buffer_bytes := PackedVector2Array(data).to_byte_array()
 	var data_buffer = rd.storage_buffer_create(data_buffer_bytes.size(), data_buffer_bytes)
 	return data_buffer
 
+func _generate_int_buffer(size):
+	var data = []
+	data.resize(size)
+	var data_buffer_bytes = PackedInt32Array(data).to_byte_array()
+	var data_buffer = rd.storage_buffer_create(data_buffer_bytes.size(), data_buffer_bytes)
+	return data_buffer
+	
 func _generate_uniform(data_buffer, type, binding):
 	var data_uniform = RDUniform.new()
 	data_uniform.uniform_type = type
@@ -194,7 +327,9 @@ func _generate_parameter_buffer(delta):
 		separation_factor,
 		get_viewport_rect().size.x,
 		get_viewport_rect().size.y,
-		delta]).to_byte_array()
+		delta,
+		pause,
+		boid_color_mode]).to_byte_array()
 	
 	return rd.storage_buffer_create(params_buffer_bytes.size(), params_buffer_bytes)
 
@@ -206,6 +341,18 @@ func _exit_tree():
 		rd.free_rid(params_buffer)
 		rd.free_rid(boid_pos_buffer)
 		rd.free_rid(boid_vel_buffer)
-		rd.free_rid(pipeline)
+		rd.free_rid(bin_buffer)
+		rd.free_rid(bin_sum_buffer)
+		rd.free_rid(bin_prefix_sum_buffer)
+		rd.free_rid(bin_index_tracker_buffer)
+		rd.free_rid(bin_reindex_buffer)
+		rd.free_rid(bin_params_buffer)
+		rd.free_rid(bin_sum_pipeline)
+		rd.free_rid(bin_sum_shader)
+		rd.free_rid(bin_prefix_sum_pipeline)
+		rd.free_rid(bin_prefix_sum_shader)
+		rd.free_rid(bin_reindex_pipeline)
+		rd.free_rid(bin_reindex_shader)
+		rd.free_rid(boid_pipeline)
 		rd.free_rid(boid_compute_shader)
 		rd.free()
